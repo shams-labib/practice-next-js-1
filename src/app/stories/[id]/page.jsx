61ef7972159ebbc42b06f23a0c72c917e88a7233
shams al labib <samslabib19@gmail.com>
2026-03-26 17:42:34 +0600

    added

---
+++ src/app/stories/[id]/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const StoryDetailsPage = () => {
+  return (
+    <div>
+      <h1>This is story details page</h1>
+    </div>
+  );
+};
+
+export default StoryDetailsPage;
